0a10b50e408be1ea303990dd146ca48a9215ff43
Thomas Yu <hey@thomassyy.com>
2026-06-21 19:35:40 -0400

    nvim: clean up monokai

--- .config/nvim/lua/plugins/monokai-pro.lua
+++ .config/nvim/lua/plugins/monokai-pro.lua
@@ -1,44 +1,10 @@
--- -- https://github.com/loctvl842/monokai-pro.nvim
--- return {
---   "loctvl842/monokai-pro.nvim",
---   lazy = false,
---   priority = 1000,
---   opts = {
---     -- filter = classic | octagon | pro | machine | ristretto | spectrum
---     day_night = {
---       enable = true,
---       day_filter = "machine",
---       night_filter = "machine",
---     },
---     background_clear = {
---       "which-key",
---     },
---     override = function(c)
---       local mc = require("mini.colors")
---
---       return {
---         -- vim.o.colorcolumn, slightly darker than the bg color
---         ColorColumn = {
---           bg = mc.modify_channel(c.editor.background, "lightness", function(l)
---             return l - 1
---           end),
---         },
---         --
---         Directory = { fg = c.base.cyan, bg = c.editor.background, bold = true },
---
---         -- Search = { bg = c.base.dimmed3 },
---       }
---     end,
---   },
---   init = function()
---     vim.cmd([[colorscheme monokai-pro]])
---   end,
--- }
-
 -- https://github.com/khoido2003/monokai-v2.nvim
 -- IMPORTANT: Must run :MonokaiCache clear command after making any changes.
 return {
   "khoido2003/monokai-v2.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
   priority = 1000,
   opts = {
     -- classic | octagon | pro | machine | ristretto | spectrum
