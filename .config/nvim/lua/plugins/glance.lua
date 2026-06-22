6552471dcc9d030d584ca28c86c4d21415cf8c83
Thomas Yu <hey@thomassyy.com>
2026-06-22 09:23:41 -0400

    nvim: trying out overlook and glance

---
+++ .config/nvim/lua/plugins/glance.lua
@@ -0,0 +1,13 @@
+-- https://github.com/DNLHC/glance.nvim
+return {
+  "dnlhc/glance.nvim",
+  cmd = "Glance",
+  config = function()
+    require("glance").setup()
+
+    -- vim.keymap.set("n", "gD", "<CMD>Glance definitions<CR>")
+    -- vim.keymap.set("n", "gR", "<CMD>Glance references<CR>")
+    -- vim.keymap.set("n", "gY", "<CMD>Glance type_definitions<CR>")
+    -- vim.keymap.set("n", "gM", "<CMD>Glance implementations<CR>")
+  end,
+}
---
+++ .config/nvim/lua/plugins/overlook.lua
@@ -0,0 +1,24 @@
+-- https://github.com/WilliamHsieh/overlook.nvim
+return {
+  "WilliamHsieh/overlook.nvim",
+  -- enabled = false,
+  config = function()
+    require("overlook").setup({
+      ui = {
+        size_ratio = 0.95,              -- Default size ratio (0.0 to 1.0)
+      },
+    })
+
+    local api = require("overlook.api")
+    vim.keymap.set("n", "gpd", api.peek_definition, { desc = "Peek definition" })
+    vim.keymap.set("n", "gpp", api.peek_cursor, { desc = "Peek cursor" })
+    vim.keymap.set("n", "gpu", api.restore_popup, { desc = "Restore last popup" })
+    vim.keymap.set("n", "gpU", api.restore_all_popups, { desc = "Restore all popups" })
+    vim.keymap.set("n", "gpc", api.close_all, { desc = "Close all popups" })
+    vim.keymap.set("n", "gpf", api.switch_focus, { desc = "Switch focus" })
+    vim.keymap.set("n", "gps", api.open_in_split, { desc = "Open popup in split" })
+    vim.keymap.set("n", "gpv", api.open_in_vsplit, { desc = "Open popup in vsplit" })
+    vim.keymap.set("n", "gpt", api.open_in_tab, { desc = "Open popup in tab" })
+    vim.keymap.set("n", "gpo", api.open_in_original_window, { desc = "Open popup in curr window" })
+  end,
+}
