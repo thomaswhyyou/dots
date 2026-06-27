e24529a339d0e36694808f9b8322faae6a83d346
Thomas Yu <hey@thomassyy.com>
2026-06-27 02:24:08 -0400

    nvim: use the forked overlook

--- .config/nvim/lua/plugins/overlook.lua
+++ .config/nvim/lua/plugins/overlook.lua
@@ -1,21 +1,45 @@
 -- https://github.com/WilliamHsieh/overlook.nvim
 return {
-  "WilliamHsieh/overlook.nvim",
-  -- enabled = false,
+  -- "WilliamHsieh/overlook.nvim",
+  "thomaswhyyou/overlook.nvim",
   config = function()
     require("overlook").setup({
       ui = {
-        size_ratio = 1, -- Default size ratio (0.0 to 1.0)
+        title_pos = "left", -- added in the fork
+        size_ratio = 1,
         row_offset = 0,
         col_offset = 0,
         min_width = 120,
       },
     })
 
+    -- -- Overlook hardcodes `title_pos = "center"` (popup.lua) with no config
+    -- -- option. Wrap popup.new so each popup left-aligns its title after opening.
+    -- local popup = require("overlook.popup")
+    -- local orig_new = popup.new
+    -- popup.new = function(...)
+    --   local p = orig_new(...)
+    --   if type(p) == "table" then
+    --     local orig_open = p.open
+    --     p.open = function(self, enter)
+    --       local ok = orig_open(self, enter)
+    --       if ok and self.winid and vim.api.nvim_win_is_valid(self.winid) then
+    --         local cfg = vim.api.nvim_win_get_config(self.winid)
+    --         pcall(vim.api.nvim_win_set_config, self.winid, {
+    --           title = cfg.title,
+    --           title_pos = "left",
+    --         })
+    --       end
+    --       return ok
+    --     end
+    --   end
+    --   return p
+    -- end
+
     local api = require("overlook.api")
     vim.keymap.set("n", "gD", api.peek_definition, { desc = "Peek definition" })
-    vim.keymap.set("n", "gpd", api.peek_definition, { desc = "Peek definition" })
 
+    vim.keymap.set("n", "gpd", api.peek_definition, { desc = "Peek definition" })
     vim.keymap.set("n", "gpp", api.peek_cursor, { desc = "Peek cursor" })
     vim.keymap.set("n", "gpu", api.restore_popup, { desc = "Restore last popup" })
     vim.keymap.set("n", "gpU", api.restore_all_popups, { desc = "Restore all popups" })
