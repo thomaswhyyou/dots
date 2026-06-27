2b6710792a0aab270632d14e044ff96a8f6d85c0
Thomas Yu <hey@thomassyy.com>
2026-06-27 02:23:32 -0400

    nvim: update smart-splits

--- .config/nvim/lua/plugins/smart-splits.lua
+++ .config/nvim/lua/plugins/smart-splits.lua
@@ -3,25 +3,38 @@
 return {
   "mrjones2014/smart-splits.nvim",
   config = function()
-    require("smart-splits").setup({
+    local ss = require("smart-splits")
+
+    ss.setup({
       at_edge = "stop",
       cursor_follows_swapped_bufs = true,
     })
+
+    -- Don't move out of the overlook popups.
+    local function maybe_move(move)
+      return function()
+        if vim.w.is_overlook_popup then
+          return
+        end
+        move()
+      end
+    end
+
     -- resizing splits
-    vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
-    vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
-    vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
-    vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
+    vim.keymap.set("n", "<A-h>", ss.resize_left)
+    vim.keymap.set("n", "<A-j>", ss.resize_down)
+    vim.keymap.set("n", "<A-k>", ss.resize_up)
+    vim.keymap.set("n", "<A-l>", ss.resize_right)
     -- moving between splits
-    vim.keymap.set({ "n", "t" }, "<C-h>", require("smart-splits").move_cursor_left)
-    vim.keymap.set({ "n", "t" }, "<C-j>", require("smart-splits").move_cursor_down)
-    vim.keymap.set({ "n", "t" }, "<C-k>", require("smart-splits").move_cursor_up)
-    vim.keymap.set({ "n", "t" }, "<C-l>", require("smart-splits").move_cursor_right)
-    vim.keymap.set({ "n", "t" }, "<C-\\>", require("smart-splits").move_cursor_previous)
+    vim.keymap.set({ "n", "t" }, "<C-h>", maybe_move(ss.move_cursor_left))
+    vim.keymap.set({ "n", "t" }, "<C-j>", maybe_move(ss.move_cursor_down))
+    vim.keymap.set({ "n", "t" }, "<C-k>", maybe_move(ss.move_cursor_up))
+    vim.keymap.set({ "n", "t" }, "<C-l>", maybe_move(ss.move_cursor_right))
+    vim.keymap.set({ "n", "t" }, "<C-\\>", maybe_move(ss.move_cursor_previous))
     -- swapping buffers between windows
-    vim.keymap.set("n", "<C-w>H", require("smart-splits").swap_buf_left)
-    vim.keymap.set("n", "<C-w>J", require("smart-splits").swap_buf_down)
-    vim.keymap.set("n", "<C-w>K", require("smart-splits").swap_buf_up)
-    vim.keymap.set("n", "<C-w>L", require("smart-splits").swap_buf_right)
+    vim.keymap.set("n", "<C-w>H", ss.swap_buf_left)
+    vim.keymap.set("n", "<C-w>J", ss.swap_buf_down)
+    vim.keymap.set("n", "<C-w>K", ss.swap_buf_up)
+    vim.keymap.set("n", "<C-w>L", ss.swap_buf_right)
   end,
 }
