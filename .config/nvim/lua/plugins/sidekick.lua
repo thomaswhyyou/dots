93d1df1c284131342867e1fa5551095f033c9511
Thomas Yu <hey@thomassyy.com>
2026-07-01 01:58:51 -0400

    nvim: clean up sidekick config

--- .config/nvim/lua/plugins/sidekick.lua
+++ .config/nvim/lua/plugins/sidekick.lua
@@ -19,21 +19,27 @@ return {
   keys = {
     {
       "<c-.>",
-      -- function() require("sidekick.cli").toggle() end,
       function() require("sidekick.cli").toggle({ filter = { installed = true } }) end,
       desc = "Sidekick Toggle",
-      -- mode = { "n", "t", "i", "x" },
     },
     {
-      "<leader>aa",
-      function() require("sidekick.cli").toggle({ filter = { installed = true } }) end,
-      desc = "Sidekick Toggle CLI",
+      "<leader>as",
+      function()
+        require("sidekick.cli").select({ filter = { installed = true } })
+      end,
+      desc = "Sidekick Select",
     },
+    -- {
+    --   "<leader>aa",
+    --   function() require("sidekick.cli").toggle({ filter = { installed = true } }) end,
+    --   desc = "Sidekick Toggle CLI",
+    -- },
     -- { "<leader>ad", function() require("sidekick.cli").close() end, desc = "Detach a CLI Session" },
     { "<leader>at", function() require("sidekick.cli").send({ msg = "{this}" }) end, mode = { "x", "n" }, desc = "Send This" },
     { "<leader>af", function() require("sidekick.cli").send({ msg = "{file}" }) end, desc = "Send File" },
     { "<leader>av", function() require("sidekick.cli").send({ msg = "{selection}" }) end, mode = { "x" }, desc = "Send Visual Selection" },
     { "<leader>ap", function() require("sidekick.cli").prompt() end, mode = { "n", "x" }, desc = "Sidekick Select Prompt" },
+
     -- Example of a keybinding to open Claude directly
     {
       "<leader>ac",
