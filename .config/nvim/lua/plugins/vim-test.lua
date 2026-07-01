28c65f0bc51f8ada50e6e2275a5a4fad4d34af9e
Thomas Yu <hey@thomassyy.com>
2026-07-01 01:27:22 -0400

    nvim: update vim-test configs

--- .config/nvim/lua/plugins/vim-test.lua
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -1,11 +1,17 @@
--- https://github.com/vim-test/vim-test
 return {
-  "vim-test/vim-test",
-  dependencies = {
-    "akinsho/toggleterm.nvim",
-  },
+  "https://github.com/vim-test/vim-test",
   init = function()
-    vim.g["test#strategy"] = "toggleterm"
+    vim.g["test#custom_strategies"] = {
+      snacks = function(cmd)
+        require("snacks").terminal.open(cmd, {
+          -- Non-interactive: no auto-insert and, crucially, no auto-close so
+          -- the test output stays on screen after the run finishes.
+          interactive = false,
+          win = { position = "bottom" },
+        })
+      end,
+    }
+    vim.g["test#strategy"] = pcall(require, "snacks") and "snacks" or "neovim"
   end,
   cmd = {
     "TestNearest",
@@ -16,9 +22,9 @@ return {
   },
   keys = {
     { "<leader>tn", "<cmd>TestNearest<CR>" },
-    -- { "<leader>tf", "<cmd>TestFile<CR>" },
-    -- { "<leader>ts", "<cmd>TestSuite<CR>" },
-    -- { "<leader>tl", "<cmd>TestLast<CR>" },
-    -- { "<leader>tg", "<cmd>TestVisit<CR>" },
+    { "<leader>tf", "<cmd>TestFile<CR>" },
+    { "<leader>ts", "<cmd>TestSuite<CR>" },
+    { "<leader>tl", "<cmd>TestLast<CR>" },
+    { "<leader>tg", "<cmd>TestVisit<CR>" },
   },
 }
