e7505942cde363c4fdf96029b7188255725d0f24
Thomas Yu <hey@thomassyy.com>
2026-07-04 01:03:46 -0400

    nvim: remove vim-fugitive

--- .config/nvim/lua/plugins/vim-fugitive.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "https://github.com/tpope/vim-fugitive",
-  enabled = false,
-  lazy = false,
-  -- cmd = { "Git", "G", "Gdiffsplit", "Gread", "Gwrite", "Gblame", "Gedit" },
-  cmd = { "G" }
-}
