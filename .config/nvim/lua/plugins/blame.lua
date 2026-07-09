3eabf4723cfe651e0b5aef07435872e5f9dcadbd
Thomas Yu <hey@thomassyy.com>
2026-07-09 01:04:30 -0400

    nvim: update blame config

--- .config/nvim/lua/plugins/blame.lua
+++ .config/nvim/lua/plugins/blame.lua
@@ -1,29 +1,27 @@
 return {
-  {
-    "https://github.com/FabijanZulj/blame.nvim",
-    lazy = false,
-    config = function()
-      require("blame").setup({
-        blame_options = { "-w" },
-        date_format = "%Y/%m/%d %H:%M",
-        -- Default mappings:
-        mappings = {
-          commit_info = "i",
-          stack_push = "<TAB>",
-          stack_pop = "<BS>",
-          show_commit = "<CR>",
-          close = { "<esc>", "q" },
-          copy_hash = "y",
-          open_in_browser = "o",
-        },
-      })
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "BlameViewOpened",
-        callback = function()
-          vim.opt_local.signcolumn = "no"
-        end,
-      })
-      vim.keymap.set("n", "<leader>gb", "<cmd>BlameToggle<cr>", { desc = "Toggle git blame" })
-    end,
-  },
+  "https://github.com/FabijanZulj/blame.nvim",
+  lazy = false,
+  config = function()
+    require("blame").setup({
+      blame_options = { "-w" },
+      date_format = "%Y-%m-%d %H:%M",
+      -- Default mappings:
+      mappings = {
+        commit_info = "i",
+        stack_push = "<TAB>",
+        stack_pop = "<BS>",
+        show_commit = "<CR>",
+        close = { "<esc>", "q" },
+        copy_hash = "y",
+        open_in_browser = "o",
+      },
+    })
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "BlameViewOpened",
+      callback = function()
+        vim.opt_local.signcolumn = "no"
+      end,
+    })
+    vim.keymap.set("n", "<leader>gb", "<cmd>BlameToggle<cr>", { desc = "Toggle git blame" })
+  end,
 }
