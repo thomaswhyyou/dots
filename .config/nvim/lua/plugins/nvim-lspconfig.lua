004b2f3b23178bcc807267c8db0a1c4de435d9dd
Thomas Yu <hey@thomassyy.com>
2026-07-12 16:50:34 -0400

    nvim: upgrade to TS v7 for lsp

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,13 @@
 return {
   "https://github.com/neovim/nvim-lspconfig",
   config = function()
+    -- TODO: Migrate once nvim-lspconfig supports TS v7+
+    vim.lsp.config("tsgo", {
+      cmd = { "tsc", "--lsp", "--stdio" },
+      filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+      root_dir = vim.fs.root(0, { "package.json", "tsconfig.json", ".git" }),
+    })
+
     vim.lsp.enable({
       "lua_ls",
       "rust_analyzer",
