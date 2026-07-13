9a8890dbed23607790436482443324f3eccc91fd
Thomas Yu <hey@thomassyy.com>
2026-07-13 00:44:57 -0400

    nvim: clean up fzf-lua configs

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -3,14 +3,8 @@ return {
   -- stylua: ignore
   keys = {
     { "<leader>p", function() require("fzf-lua").files() end, desc = "Fzf Files" },
-    { "<leader>/", function() require("fzf-lua").grep() end, desc = "Ff Grep" },
-    -- {
-    --   "gO",
-    --   function()
-    --     require("fzf-lua").lsp_document_symbols()
-    --   end,
-    --   desc = "fzf document symbols",
-    -- },
+    { "<leader>/", function() require("fzf-lua").grep() end, desc = "Fzf Grep" },
+    { "gO", function() require("fzf-lua").lsp_document_symbols() end, desc = "Fzf Document Symbols" },
   },
   config = function()
     require("fzf-lua").setup({
@@ -41,34 +35,17 @@ return {
           end
         end,
       },
-      -- Use the default fzf layout
-      fzf_opts = { ["--layout"] = "default" },
-      -- Needs this so theme colors can be respected
-      fzf_colors = true,
-
-      -- TODO: Clean up the configs
-      -- https://github.com/gennaro-tedesco/dotfiles/blob/master/nvim/lua/plugins/fzf.lua
+      fzf_colors = true, -- Needs this so theme colors can be respected
       files = {
-        -- formatter = "path.filename_first",
-        -- git_icons = false,
-        -- no_header = true,
-        -- cwd_header = false,
-        cwd_prompt = false,
-        -- cwd = require("utils").git_root(),
+        -- stylua: ignore
         actions = {
-          ["tab"] = function(_, _)
-            require("fzf-lua").buffers()
-          end,
+          ["tab"] = function(_, _) require("fzf-lua").buffers() end,
         },
       },
       buffers = {
-        -- cwd_prompt = false,
-        -- cwd = require("utils").git_root(),
+        -- stylua: ignore
         actions = {
-          ["tab"] = function(_, _)
-            -- open buffers picker
-            require("fzf-lua").files()
-          end,
+          ["tab"] = function(_, _) require("fzf-lua").files() end,
         },
       },
       grep = {
@@ -82,31 +59,3 @@ return {
     })
   end,
 }
-
--- Search
--- { '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
--- { '<leader>s/', function() Snacks.picker.search_history() end, desc = "Search History" },
--- { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
--- { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
--- { "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
--- { "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
--- { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
--- { "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
--- { "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
--- { "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
--- { "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
--- { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
--- { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
--- { "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
--- { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
--- { "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
--- { "<leader>sp", function() Snacks.picker.lazy() end, desc = "Search for Plugin Spec" },
--- { "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
--- LSP
--- { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
--- { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
--- { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
--- { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
--- { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
--- { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
--- { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
