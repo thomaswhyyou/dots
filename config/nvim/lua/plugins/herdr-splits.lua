8afb0c187992bc46971c9a20b1eeef5ff4045fe0
Thomas Yu <hey@thomassyy.com>
2026-07-20 00:53:22 -0400

    nvim: replicate swap buffers between windows

--- config/nvim/lua/plugins/herdr-splits.lua
+++ config/nvim/lua/plugins/herdr-splits.lua
@@ -24,5 +24,26 @@ return {
     vim.keymap.set("n", "<M-j>", hs.resize_down)
     vim.keymap.set("n", "<M-k>", hs.resize_up)
     vim.keymap.set("n", "<M-l>", hs.resize_right)
+    -- swapping buffers between windows
+    -- Mirrors smart-splits: at_edge = "stop", cursor_follows_swapped_bufs = true.
+    local function swap_buf(dir_key)
+      return function()
+        local cur_win = vim.api.nvim_get_current_win()
+        local cur_buf = vim.api.nvim_win_get_buf(cur_win)
+        local cur_pos = vim.api.nvim_win_get_cursor(cur_win)
+        vim.cmd("wincmd " .. dir_key)
+        local target_win = vim.api.nvim_get_current_win()
+        if target_win == cur_win then return end -- at edge: stop
+        local target_buf = vim.api.nvim_win_get_buf(target_win)
+        vim.api.nvim_win_set_buf(cur_win, target_buf)
+        vim.api.nvim_win_set_buf(target_win, cur_buf)
+        -- cursor follows the swapped buffer (target_win is now focused)
+        pcall(vim.api.nvim_win_set_cursor, target_win, cur_pos)
+      end
+    end
+    vim.keymap.set("n", "<C-w>H", swap_buf("h"))
+    vim.keymap.set("n", "<C-w>J", swap_buf("j"))
+    vim.keymap.set("n", "<C-w>K", swap_buf("k"))
+    vim.keymap.set("n", "<C-w>L", swap_buf("l"))
   end,
 }
